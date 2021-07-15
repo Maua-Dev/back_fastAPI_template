0fc84bd4d1fc1700d0d4cfa41f1fd515467eaff3
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-07-14 21:51:15 -0300

    added pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,11 @@
 from fastapi.testclient import TestClient
 
-
 from src.main import app
 
-def test_main(client):
+client = TestClient(app)
+
+
+def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    #assert response.json() == {"message": "Hello World"}
-
-
-if __name__ == '__main__':
-    client = TestClient(app)
-    test_main(client)
+    assert response.json() == {"message": "Hello World"}

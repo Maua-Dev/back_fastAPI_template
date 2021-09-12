90dd6eceac71286e355d29856bc5564ad30ee371
VitorMartin <vitor.martin.simoni@gmail.com>
2021-09-12 05:56:45 -0300

    Adaptando teste para codigo novo

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,14 @@
 from fastapi.testclient import TestClient
 
-from src.main import app
+from src.controladores.fastapi.http.respostas import ResRoot
+from src.fabricas.controladores.fastapi.fabrica_controlador_fastapi import *
+from src.main import ctrl
 
-client = TestClient(app)
+
+client = TestClient(ctrl.app)
 
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == ResRoot(deployment=ProjConfig.getDeployment(), controlador=ProjConfig.getFastapi())

47807abaff5b57486568a7f5a5521dcaa86df851
Bruno Vilardi <brunovilardibueno@gmail.com>
2021-09-15 20:55:55 -0300

    corrigindo path

--- src/config/proj_config.py
+++ src/config/proj_config.py
@@ -3,15 +3,17 @@ import os
 
 
 class ProjConfig:
-    PROJ_ROOT_ABS_PATH = os.path.abspath('.')
+    PROJ_ROOT_ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+
 
     @staticmethod
     def getDeployment():
-        return ProjConfig.fromJSON(os.path.join(ProjConfig.PROJ_ROOT_ABS_PATH, 'config', 'files', 'deployment.json'))
+        print(ProjConfig.PROJ_ROOT_ABS_PATH)
+        return ProjConfig.fromJSON(os.path.join(ProjConfig.PROJ_ROOT_ABS_PATH, 'files', 'deployment.json'))
 
     @staticmethod
     def getFastapi():
-        return ProjConfig.fromJSON(os.path.join(ProjConfig.PROJ_ROOT_ABS_PATH, 'config', 'files', 'fastapi.json'))
+        return ProjConfig.fromJSON(os.path.join(ProjConfig.PROJ_ROOT_ABS_PATH, 'files', 'fastapi.json'))
 
     @staticmethod
     def fromJSON(path):

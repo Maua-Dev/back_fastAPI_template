f6da686c5e874dabcb1c35537b6cadde410e61f0
Bruno Vilardi <brunovilardibueno@gmail.com>
2021-10-03 14:02:41 -0300

    definindo o tipo de repo e controlador no teste

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,10 +2,11 @@ from fastapi.testclient import TestClient
 
 from src.controladores.fastapi.http.respostas import ResRoot
 from src.fabricas.controladores.fastapi.fabrica_controlador_fastapi import *
+from src.config.enums.deployment import KEY
 from src.main import main
 
 
-(_, ctrl) = main()
+(_, ctrl) = main(repo=ProjConfig.getDeployment()[KEY.TIPO_REPOSITORIO.value], ctrl=ProjConfig.getDeployment()[KEY.TIPO_CONTROLADOR.value])
 client = TestClient(ctrl.app)
 
 

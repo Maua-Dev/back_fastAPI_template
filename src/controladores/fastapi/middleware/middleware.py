0e3b498937aebee4fa089d7b598a7d7ddc317fe6
FelipeGiusti-2000 <61150874+FelipeGiusti-2000@users.noreply.github.com>
2021-10-21 20:50:10 -0300

    Adicao de middleware

---
+++ src/controladores/fastapi/middleware/middleware.py
@@ -0,0 +1,20 @@
+from fastapi import Request, Response
+import requests
+
+async def add_redirect_auth(request: Request, call_next):
+    bearer = request.headers['Authorization']  # Pegamoso header
+    token = bearer.split()[1]  # separamos o token
+
+    res = await _enviaParaValidar(token)  # Enviamos para GetAuthw -> Ele joga erro se falhar
+
+    if res.status_code == 200:
+        response = await call_next(request)
+    else:
+        response = Response(content=res.content, status_code=res.status_code)
+
+    return response
+
+
+async def _enviaParaValidar(token: str):
+    task = requests.get("http://localhost:8000/validar", headers={"Authorization": "Bearer " + token})
+    return task
--- src/fabricas/controladores/fastapi/fabrica_controlador_fastapi.py
+++ src/fabricas/controladores/fastapi/fabrica_controlador_fastapi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from src.config.proj_config import ProjConfig
 from src.config.enums.fastapi import *
@@ -7,6 +8,8 @@ from src.controladores.fastapi.c_fastapi_2 import CFastapi2
 from src.controladores.fastapi.start import Start
 from src.interfaces.IRepo import IRepo
 from src.controladores.fastapi.roteadores.roteador import Roteador
+
+from src.controladores.fastapi.middleware.middleware import add_redirect_auth
 
 
 class FabricaControladorFastapi:
@@ -34,6 +37,7 @@ class FabricaControladorFastapi:
         self.url = f'{self.protocolo}://{self.host}:{self.porta}{self.root}'
 
         self.app = FastAPI()
+        self.app.add_middleware(BaseHTTPMiddleware, dispatch=add_redirect_auth)
         self.app.include_router(Roteador(self))
 
     def metodoControlador1(self) -> object:
